d7e87c238d08df5d76190a55049d78b21a7baaf4
Humairatas <71521057+Humairatas@users.noreply.github.com>
2020-09-21 01:54:20 +0600

    Alhamdulliah

---
+++ script.js
@@ -0,0 +1,419 @@
+/*var a, b, c;
+a = 10;
+b = 3;
+c = a + b;
+document.write(c);
+document.write("<br>");
+a++;
+c = a + b;
+document.write(c);
+const a = 13;
+a = 15;
+document.write(a);
+
+var x = "hello";
+x = 23;
+x = true;
+x = ["html"."CSS", "js"];
+x = {
+    name: "Obi",
+    id: "kak"
+};
+
+x = undefind;
+x;
+x = null;
+document.write(x);
+document.write("<br>");
+document.write(typeof x);
+x += y-- - x=x + y----
+
+ var x = 10;
+var y = 3;
+x %= y;
+
+document.write(x);
+
+
+document.write(4 == "4");
+if (10 > 10) {
+    document.write("greater")
+} else {
+    documevt.write("lower")
+}
+
+let a, b, c;
+a = 10;
+b = 5;
+let a = 7;
+c = a - b;
+document.write(c);
+var a, b, c;
+a = 10;
+b = 5;
+var a = 7
+c = a - b;
+document.write(c);
+
+const a = 10;
+const b = 5;
+const c = a - b;
+const a = 7;
+document.write(c);
+var x;
+document.write(x);
+document.write("<br>");
+document.write(typeof x);
+var x = 10;
+var y = 2;
+x **= y   // x=x**y
+document.write(x);
+var x = "30";
+
+document.write(typeof x);
+true false
+var a, b;
+a = 10;
+b = 20;
+console.log(a <= b); // != means Not equal
+var a = 10;
+var b = 20;
+if (a + b) {
+    document.write("b is greater ");
+} else {
+    document.write("a is not greater than b")
+}
+var a = 5;
+if (a > 10) {
+    document.write("a is greater")
+} else {
+    document.write("a is lower");
+}
+var per = 57;
+if (per >= 80 && per <= 100) {
+    document.write("A+")
+} else if (per >= 60 && per < 80) {
+    document.write("A")
+} else if (per >= 45 && per < 60) {
+    document.write("A-")
+} else if (per >= 33 && per < 45) {
+    document.write("B")
+} else if (per < 33) {
+    document.write("Fail")
+} else {
+    document.write("Please enter valid number")
+}
+var day = 6;
+switch (day) {
+    case 1:
+        document.write("Today is Saturday");
+        break;
+    case 2:
+        document.write("Today is Sunday");
+        break;
+    case 3:
+        document.write("Today is Monday");
+        break;
+    case 4:
+        document.write("Today is Tuesday");
+        break;
+    case 5:
+        document.write("Today is Wednesday");
+        break;
+    case 6:
+        document.write("Today is Thursday");
+        break;
+    case 7:
+        document.write("Today is Friday");
+        break;
+
+    default:
+        document.write("Enter the valid date");
+        break;
+}
+var age = 15;
+switch (false) {
+    case (age > 14 && age <= 20):
+        document.write("You are not eligble");
+        break;
+    case (age > 20 && age <= 30):
+        document.write("You are eligble");
+        break;
+
+    default:
+        document.write("Enter the valid date")
+        break;
+}
+var a = 20;
+if (a > 30) {
+    document.write("true");
+} else {
+    document.write("false");
+}
+var per = 50;
+if (per >= 80 && per <= 100) {
+    document.write("A+");
+} else if (per >= 60 && per < 80) {
+    document.write("A");
+} else if (per >= 45 && per < 59) {
+    document.write("A-");
+} else if (per >= 33 && per < 45) {
+    document.write("B");
+} else if (per < 33) {
+    document.write("Fail")
+} else {
+    document.writr("Please enter valid number ");
+}
+var day = 6;
+switch (day) {
+    case: 1
+        document.writr("Today is Saturday");
+
+        break;
+    case: 2
+        document.writr("Today is Sunday");
+
+        break;
+    case: 3
+        document.writr("Today is Monday");
+
+        break;
+    case: 4
+        document.writr("Today is Tuesday");
+
+        break;
+    case: 5
+        document.writr("Today is Wednesday");
+
+        break;
+    case: 6
+        document.writr("Today is Thursday");
+
+        break;
+
+    default;
+        document.write("Please enter valid Date");
+        break;
+}
+var age = 37;
+switch (true) {
+    case (age > 14 && age <= 20):
+        document.write("You are noy eligble");
+        break;
+    case (age > 20 && age <= 30):
+        document.write("You are eligble");
+        break;
+
+    default:
+        document.write("Wrong valid");
+        break;
+}
+var a = 10;
+var b = 20;
+if (a > b) {
+    alert("a is greater");
+} else {
+    alert("b is greater")
+}
+var a = promot("What is your name?");
+console.log(a);
+
+function hello() {
+    document.write("Hello World");
+    document.write("<br>");
+}
+hello();
+hello();
+hello();
+hello();
+hello();
+hello();
+
+function su(a, b) {
+    document.write(a + b);
+
+}
+sum(20, 30);
+
+
+function sum() {
+    var a = 30;
+    document.write(a);
+
+
+}
+sum();
+
+var per = prompt("Enter Percentage");
+if (per >= 80 && per <= 100) {
+    document.write("A+")
+} else if (per >= 60 && per <= 80) {
+    document.write("A")
+} else if (per >= 45 && per <= 60) {
+    document.write("A-")
+} else if (per >= 33 && per <= 45) {
+    document.write("B")
+} else if (per < 33) {
+    document.write("Fail")
+} else {
+    document.write("Please enter valid number");
+}
+
+function hello() {
+    document.write("Hellow Everyone");
+
+}
+
+
+var per = prompt("Enter your persentage");
+if (per >= 80 && per <= 100) {
+    document.write("A+")
+} else if (per >= 60 && per <= 80) {
+    document.write("A")
+} else if (per >= 45 && per <= 60) {
+    document.write("A-")
+} else if (per >= 33 && per <= 45) {
+    document.write("B")
+} else if (per < 33) {
+    document.write("Fail")
+} else {
+    document.write("Please enter valid number");
+}
+var a = pormpt("What is your name?");
+documevt.write(a);
+
+function hello() {
+    documevt.write("hello world");
+    documevt.write("<br>");
+
+}
+hello();
+hello();
+hello();
+hello();
+hello();
+
+function sum(a, b, c) {
+    document.write(a - b + c);
+    document.write("<br>")
+
+    }
+sum(20,10,30);
+
+
+function sum () {
+var a=10;
+document.write(a);
+document.write("<br>");
+
+}
+sum();
+document.write(a);
+
+function hello() {
+    document.write("Welcome");
+}
+loop
+var a=10;
+initialization
+condition true increment decrement
+statement
+
+a++ a= a+1
+ 10 9 8 7 6 5 4 3 2 1 
+ if true false 
+ while loop
+ do/while loop
+ for loop
+ for/ inloop (object)
+ for each(array)
+
+
+
+
+ document.write("hello world");
+ document.write("hello world");
+ document.write("hello world");
+ document.write("hello world");
+ document.write("hello world");
+
+ var a=10;
+ document.write("<ul>");
+ while (a>=1) {
+     document,write("<li>" + a + ") hello world <br> "+"</li>") ;
+     a=a-1
+
+ }
+ document,write("</ul>");
+
+
+ var a=1 ;
+ d0{
+    document,write(a+") hello world <br>") ;
+    a++
+ } while (a<=10);
+
+ for( var a=1 ; a<=10; a++ ){
+    document,write(a + ") hello world <br>");
+ }
+
+ for ( var a=1 ; a<=10; a++) {
+    if(a==3){
+        document,write( a +") Hey <br>");
+
+
+    }
+    document,write(a + ") Hello world <br>")
+ }
+
+ for (var a=5; a<=10; a++) {
+     if (a%2==0){
+        document,write(a+ "<br>");
+     }
+ }
+
+ var a= [10,20,30,40,50,60];
+ document,write(a[3]);
+
+ var a=10;
+ document,write("<ul>");
+ while (a>=1) {
+    document,write("<li>" + a +") hello world <br>" + "</li>");
+a=a-1;
+}
+document,write("<ul>");
+
+
+var a=1;
+do {
+    document,write(a + ") hello world <br>");
+    a=a+1;
+} while (a<=10);
+
+
+for (var a=1; a<=10; a++) {
+    document,write( a+") hello world <br>");
+    }
+
+    for (var a=1; a<=10; a++){
+        if (a==6){
+            document,write(a + ") hey <br>");
+            break
+}
+document,write( a +") hello world <br>");
+
+    }
+
+    for (var a=1; a<10; a++){
+        if (a%2==0){
+            document,write(a + ") even <br>);
+        }
+    }
+
+    //string ..number .. bollen .. null ..undefind .. array.
+    //var a=10;
+    //var a = "10, 20, 30";
+
+    var a = [10,20,30,40];
+    document,write(a[3]);
+    */
